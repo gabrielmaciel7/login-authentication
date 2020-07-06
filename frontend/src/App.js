bbdce58c66c9604d55fd601542483badc6ecd15f
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-07-06 14:08:45 -0300

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <h1>Login</h1>;
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sign-in">Sign-in</Link>
+              <Link to="/sign-up">Sign-up</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/sign-in">
+            <h1>Sign-in</h1>
+          </Route>
+          <Route path="/sign-up">
+            <h1>Sign-up</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
